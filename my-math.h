de32bcbb59b90c8129e21c3a2ee7043c4c1239d3
mlestes1 <murray.l.estes@gmail.com>
2020-05-09 08:21:12 -0500

    Inital my-math code

---
+++ my-math.h
@@ -0,0 +1,27 @@
+#ifndef __MY_MATH__
+#define __MY_MATH__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//To use: cast int/float to double
+
+//absolute value
+double my_abs(double x);
+
+//rounds a double up to the next integer
+int my_ceil(double x);
+
+//rounds a double down to the next integer
+int my_floor(double x);
+
+//truncate
+int my_trunc(double x);
+
+//determines the maximum value
+double my_max(double a, double b);
+
+//determines the minimum value
+double my_min(double a, double b);
+
+#endif
