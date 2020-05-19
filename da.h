b4311e2bc68ecbf63aa3f188090874c84168f35b
mlestes1 <murray.l.estes@gmail.com>
2020-05-18 22:04:33 -0500

    added da.c da.h

---
+++ da.h
@@ -0,0 +1,19 @@
+#ifndef __DA_H__
+#define __DA_H__
+
+#define DEFAULT 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct DynamicArray d_arr_t;
+d_arr_t *newDynamicArray(int size, void (*p)(FILE *, void *));
+int insertDynamicArray(d_arr_t *arr, void *item);
+void *setDynamicArray(d_arr_t *arr, void *item, int index);
+void *deleteObjectDynamicArray(d_arr_t *arr, int index);
+void deleteDynamicArray(d_arr_t *arr);
+void printDynamicArray(d_arr_t *arr, FILE *fp);
+int sizeDynamicArray(d_arr_t *arr);
+int capacityDynamicArray(d_arr_t *arr);
+
+#endif
