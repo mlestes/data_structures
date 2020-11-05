eb57e766e8537e9bca4e18e9739ec589b2fa63c9
mlestes <murray.l.estes@gmail.com>
2020-11-05 03:11:49 -0600

    Create stk

---
+++ stk.h
@@ -0,0 +1,18 @@
+#ifndef __MY_STK__
+#define __MY_STK__
+
+/*** INCLUDES ***/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/*** PUBLIC INTERFACE ***/
+typedef struct STACK_TYPE stk_t;
+
+stk_t *newStack(void (*p)(FILE *, void *));
+void pushStack(stk_t *s, void *v);
+void *popStack(stk_t *s);
+int sizeStack(stk_t *s);
+void printStack(stk_t *s, FILE *fp);
+
+#endif
